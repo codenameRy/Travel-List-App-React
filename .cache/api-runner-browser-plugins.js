fabb8833dc64f40b1cd5e6c7b6668127203ec1c2
Ryan Lee <ryanm.lee11@gmail.com>
2020-09-02 02:14:53 -0400

    First update

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Gatsby Starter Leaflet","short_name":"Gatsby Starter Leaflet","start_url":"/","icon":"src/assets/images/react-leaflet-icon.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"dda9d5637adfc81f012fb809eec58896"},
+    }]
