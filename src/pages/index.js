2badc558b52b08533133ebee1ffb67a3579a7359
Ryan Lee <ryanm.lee11@gmail.com>
2020-09-03 10:52:37 -0400

    Gatsby Leaflet Map update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,36 @@
-import React from "react";
-import { Helmet } from "react-helmet";
-import { Marker, Popup } from "react-leaflet";
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Marker } from 'react-leaflet';
 
-import { useDestinations } from "hooks";
-
-import Layout from "components/Layout";
-import Container from "components/Container";
-import Map from "components/Map";
-import Snippet from "components/Snippet";
+import Layout from 'components/Layout';
+import Container from 'components/Container';
+import Map from 'components/Map';
+import Snippet from 'components/Snippet';
 
 const LOCATION = {
-  lat: 0,
-  lng: 0,
+  lat: 38.9072,
+  lng: -77.0369,
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
 
 const IndexPage = () => {
-  const { destinations } = useDestinations();
 
   /**
    * mapEffect
    * @description Fires a callback once the page renders
+   * @example Here this is and example of being used to zoom in and set a popup on load
    */
 
   async function mapEffect({ leafletElement: map } = {}) {
     if ( !map ) return;
+
+
   }
 
   const mapSettings = {
     center: CENTER,
-    defaultBaseMap: "Mapbox",
+    defaultBaseMap: 'OpenStreetMap',
     zoom: DEFAULT_ZOOM,
     mapEffect,
   };
@@ -42,38 +42,14 @@ const IndexPage = () => {
       </Helmet>
 
       <Map {...mapSettings}>
-        {destinations.map(destination => {
-          const { id, location, name } = destination;
-          const { latitude, longitude } = location;
-          const position = [latitude, longitude];
-          return (
-            <Marker key={id} position={position}>
-              <Popup>{name}</Popup>
-            </Marker>
-          )
-        })}
+        <Marker position={CENTER} />
       </Map>
 
-      <Container type="content" className="text-center home-destinations">
-        <h2>Destinations</h2>
-        <ul>
-          {destinations.map((destination) => {
-            const { id, name } = destination;
-            return <li key={id}>{name}</li>;
-          })}
-        </ul>
-      </Container>
-
-      <Container type="content-full" className="text-center home-start">
-        <h2>Want to create your own mapping app?</h2>
+      <Container type="content" className="text-center home-start">
+        <h2>Still Getting Started?</h2>
         <p>Run the following in your terminal!</p>
-        <Snippet>
-          gatsby new [directory]
-          https://github.com/colbyfayock/gatsby-starter-leaflet
-        </Snippet>
-        <p className="note">
-          Note: Gatsby CLI required globally for the above command
-        </p>
+        <Snippet>gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-leaflet</Snippet>
+        <p className="note">Note: Gatsby CLI required globally for the above command</p>
       </Container>
     </Layout>
   );
